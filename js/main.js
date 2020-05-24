daf10167ee0a69ff837c525be8034a7261017eb8
Kuniyoshi Kenichiro <kuni.0518planetarium@gmail.com>
2020-05-24 23:56:49 +0900

    make function

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@
     // const diary = document.getElementsByClassName('diary');
     const hr = document.getElementsByTagName('hr');
 
+
+    function maketitle(title){
+        title.classList.add();
+    };
+
+
+
     window.onload = function() {
         // hr.classList.add('changehr');
         ttl.classList.add('changemain');
